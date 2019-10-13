3e9fe6464a6adae059be0bccaf2b452bfdb1e02b
andrew <andrewjthomsen@yahoo.com>
2019-10-13 13:12:43 -0700

    Started styling by adding a google font to applixation

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import WelcomeMessage from './WelcomeMessage';
 
 function App() {
   return (
-    <div className="App">
-    Hello World!
+    <div>
+    <WelcomeMessage/>
     </div>
   );
 }
