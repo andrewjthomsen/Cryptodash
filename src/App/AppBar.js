233698157ed6e97225fc11f95958251ce27d40a2
andrew <andrewjthomsen@yahoo.com>
2019-10-13 14:25:10 -0700

    Added further styling to navbar with margin styling, text-shadow ect...

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
+const Logo = styled.div`
+   font-size: 1.5em;
+`
 const Bar = styled.div`
 display: grid;
+margin-bottom: 40px;
 grid-template-columns: 100px auto 100px 100px;
 `
+// Styled component for button
+// When certain props are passed in, button will respond differently with specific styles
+const ControlButtonElement = styled.div`
+   cursor: pointer;
+    ${props => props.active && css`
+      text-shadow: 0px 0px 60px #03ff03;
+    `}
+`
+// Takes first character and transforms it to be uppercase
+// Styling is being applied to dashboard and settings control buttons
+function toProperCase(lower) {
+   return lower.charAt(0)
+   .toUpperCase()
+   + lower.substr(1);
+
+}
+
+// Wrapper to apply functionality and styling to buttons
+function ControlButton({ name, active }) {
+   return (
+      <ControlButtonElement active={active}>
+         {toProperCase(name)}
+      </ControlButtonElement>
+   )
+}
 
 export default function(){
-   return <Bar>
-   <div> CryptoDash </div>
-   <div/>
-   <div> Dashboard </div>
-   <div> Settings </div>
+return (
+   <Bar>
+      <Logo> CryptoDash </Logo>
+      <div/>
+      <ControlButton active name="dashboard"/> 
+      <ControlButton name="settings"/> 
    </Bar>
+   )
 }
