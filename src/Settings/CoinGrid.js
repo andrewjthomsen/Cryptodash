0d60e1021ca6f1be987715a2314deff5a936f41c
andrew <andrewjthomsen@yahoo.com>
2019-10-15 16:06:58 -0700

    Worked on styling in CoinTile, and created CoinImage to display coin images on dashboard

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import {AppContext} from '../App/AppProvider';
-import { SelectableTile } from '../Shared/Tile';
-
+import CoinTile from './CoinTile';
 
 // Divides screen into 5 divs and stretches widths out
 export const CoinGridStyled = styled.div`
     display: grid;
     grid-template-columns: repeat(5, 1fr);
     grid-gap: 15px;
+    margin-top: 40px;
 `
+function getCoinsToDisplay(coinList) {
+    return Object.keys(coinList).slice(0, 100);
+}
+
 // Taking array of keys of coinList
 // then mapping list of keys to a div
 // Each coinKey gets its own div
@@ -17,8 +21,9 @@ export const CoinGridStyled = styled.div`
 export default function() {
     return <AppContext.Consumer>
         {({ coinList })  => <CoinGridStyled>
-            {Object.keys(coinList).map(coinKey => 
-                <SelectableTile> {coinKey} </SelectableTile>)}
+            {getCoinsToDisplay(coinList).map(coinKey => 
+                <CoinTile coinKey = {coinKey}/>
+            )}
         </CoinGridStyled>}
     </AppContext.Consumer>
 }
