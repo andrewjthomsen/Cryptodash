2c8d50efc8d2f0786fe93a858b247653df826bcd
andrew <andrewjthomsen@yahoo.com>
2019-10-24 20:30:02 -0700

    Added styling to highcharts grid

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -3,7 +3,9 @@ import { Tile } from "../Shared/Tile";
 import { AppContext } from "../App/AppProvider";
 import highchartsConfig from "./HighchartsConfig";
 import ReactHighcharts from "react-highcharts";
+import HighchartsTheme from "./HighchartsTheme";
 
+ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
 export default function() {
   return (
     <AppContext.Consumer>
